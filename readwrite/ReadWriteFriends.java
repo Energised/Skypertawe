54bf042b6ca742a9e413a303b835760eb9bc0f40
Energised <woolsey64@gmail.com>
2016-12-06 19:32:23 +0000

    rwf designed

--- readwrite/ReadWriteFriends.java
+++ readwrite/ReadWriteFriends.java
@@ -5,6 +5,25 @@
 * Allows the storage of friendships between accounts in a database to be used
 * by Graph to display friends instead of a contact list for each user
 *
+* Needed functions:
+*   find_pending_requests() -> return a list of pending friend requests :: ArrayList<(Account1, Account2)>
+*   check_friend_status(Account1, Account2) -> returns the number of columns these two accounts share
+*
+*
+* Writing Friendships:
+*   -> some user adds another user
+*   -> insert tuple into database
+*   -> check if its a friend request or acceptance
+*   -> if acceptance (count is 2) then update the graph with that
+*   -> if request (count is 1) then send a notification to the user (either do this with Graph or with ProfileGUI)
+*
+* Reading friendships:
+*   -> get a table of all the friendships (where count is 2):
+*        write sql statement
+*   -> return a list of pairs of Account objects that are friends we can compare in Graph.java
+*   -> run this everytime a new friendship is made
+*
+*
 * USAGE:
 *   javac ReadWriteFriends.java
 *   java -cp ".;sqlite-jdbc-3.15.1.jar" ReadWriteFriends
