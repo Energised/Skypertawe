e2d595d869ab7736b340eee43e47c1c4885443c3
Ricky3038 <ricky3038@live.co.uk>
2016-12-11 15:11:18 +0000

    Update BSTNode.java

--- readwrite/BSTNode.java
+++ readwrite/BSTNode.java
@@ -1,21 +1,37 @@
-
+/**
+ * the BSTNode objects will be used to make up a BST.
+ * the BSTNodes contain an element of type Account, and a left/right references of type BSTNode
+ * 
+ * @author 830169
+ * @version 1.0
+ */
 public class BSTNode {
 	private Account element;
 	private BSTNode left;
 	private BSTNode right;
 	
-	public BSTNode(Account m){
-		this.element = m;
+	/**
+	 * constructor for BSTNode
+	 * @param account the account that the element will reference
+	 */
+	public BSTNode(Account account){
+		this.element = account;
 		this.left = null;
 		this.right = null;
 	}
 
 
-	
-	public void setAccount (Account m){
-		this.element = m;
+	/**
+	 * setter for the element of the BSTNode
+	 * @param account the new Account that the element attribute will reference
+	 */
+	public void setAccount (Account account){
+		this.element = account;
 	}
-	
+	/**
+	 * getter for the Account that the element attribute references
+	 * @return the Account object of this node
+	 */
 	public Account getAccount (){
 		if(this.element != null){
 			return this.element;
@@ -24,7 +40,11 @@ public class BSTNode {
 			return null;
 		}
 	}
-
+	
+	/**
+	 * getter for the node to the left of this node
+	 * @return reference to the left node
+	 */
 	public BSTNode getLeft (){
 		if(this.left != null){
 			return this.left;
@@ -34,6 +54,10 @@ public class BSTNode {
 		}
 	}
 	
+	/**
+	 * getter for node to the right of this node
+	 * @return reference to the right node
+	 */
 	public BSTNode getRight (){
 		if(this.right != null){
 			return this.right;
@@ -43,10 +67,18 @@ public class BSTNode {
 		}
 	}
 	
+	/**
+	 * setter for the left node
+	 * @param l reference to the new left node
+	 */
 	public void setLeft (BSTNode l){
 		this.left = l;
 	}
 	
+	/**
+	 * setter for the right node
+	 * @param r reference to the new right node
+	 */
 	public void setRight (BSTNode r){
 		this.right = r;
 	}
