888e953f5d013636d6f5c0287b84c11e670998e3
CarlNotKarl <c_thomas92@hotmail.co.uk>
2016-12-11 16:04:21 +0000

    Add files via upload

--- readwrite/CreateAccountGUI.java
+++ readwrite/CreateAccountGUI.java
@@ -1,40 +1,123 @@
-/**
-* CreateAccountGUI.java
-* @author Dan Woolsey
-*
-* Screen to take new Account information and create an Account object
-*/
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.Spring;
+import javax.swing.SpringLayout;
+import javax.swing.WindowConstants;
 
-public class CreateAccountGUI extends GUI
-{
-    public CreateAccountGUI() throws Exception
-    {
-        super();
 
-        JPanel panel = new JPanel();
-        add(panel);
+public class CreateAccountGUI extends GUI{
 
-        JLabel usernameLabel = new JLabel("Username: ");
-        JLabel firstnameLabel = new JLabel("First Name: ");
-        JLabel surnameLabel = new JLabel("Surname: ");
-        JLabel mobnumLabel = new JLabel("Mobile No: ");
-        JLabel dobLabel = new JLabel("Date of Birth: ");
-        JLabel cityLabel = new JLabel("City: ");
-        usernameLabel.setBounds(10,50,100,60);
-        panel.add(usernameLabel);
-    }
+	public CreateAccountGUI(){
 
-    public static void main(String[] args) throws Exception
-    {
-        CreateAccountGUI c = new CreateAccountGUI();
-        c.setVisible(true);
-    }
+		super();
+
+		setTitle("Create an account");
+		setSize(600, 400);
+		setBackground(Color.CYAN);
+		setResizable(false);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		String[] labels = {"Username: ", "First Name: ", "Surname: ",
+				"Mobile Number: ", "Date Of Birth: ", "City: "};
+		int input = labels.length;
+
+		JPanel create = new JPanel(new SpringLayout());
+		for (int i = 0; i < input; i++) {
+			JLabel para = new JLabel(labels[i], JLabel.TRAILING);
+			create.add(para);
+			JTextField form = new JTextField(10);
+			para.setLabelFor(form);
+			create.add(form);
+			super.add(create);
+		}
+		JButton fin = new JButton("Create");
+		fin.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent sendMessage){
+				// TODO send message to server
+			}
+		});
+		super.add(fin, BorderLayout.SOUTH);
+		CreateAccountGUI.makeForm(create,
+				input, 2,
+				6, 6,
+				20, 20);
+
+	}
+	private static SpringLayout.Constraints getConstraintsForCell(
+			int row, int col,
+			Container parent,
+			int cols) {
+		SpringLayout layout = (SpringLayout) parent.getLayout();
+		Component c = parent.getComponent(row * cols + col);
+		return layout.getConstraints(c);
+	}
+
+	public static void makeForm(Container parent,
+			int rows, int cols,
+			int initialX, int initialY,
+			int xPad, int yPad) {
+		SpringLayout layout;
+		try {
+			layout = (SpringLayout)parent.getLayout();
+		} catch (ClassCastException exc) {
+			System.err.println("The first argument to makeCompactGrid must use SpringLayout.");
+			return;
+		}
+
+		//Align all cells in each column and make them the same width.
+		Spring x = Spring.constant(initialX);
+		for (int c = 0; c < cols; c++) {
+			Spring width = Spring.constant(0);
+			for (int r = 0; r < rows; r++) {
+				width = Spring.max(width,
+						getConstraintsForCell(r, c, parent, cols).
+						getWidth());
+			}
+			for (int r = 0; r < rows; r++) {
+				SpringLayout.Constraints constraints =
+						getConstraintsForCell(r, c, parent, cols);
+				constraints.setX(x);
+				constraints.setWidth(width);
+			}
+			x = Spring.sum(x, Spring.sum(width, Spring.constant(xPad)));
+		}
+
+		//Align all cells in each row and make them the same height.
+		Spring y = Spring.constant(initialY);
+		for (int r = 0; r < rows; r++) {
+			Spring height = Spring.constant(0);
+			for (int c = 0; c < cols; c++) {
+				height = Spring.max(height,
+						getConstraintsForCell(r, c, parent, cols).
+						getHeight());
+			}
+			for (int c = 0; c < cols; c++) {
+				SpringLayout.Constraints constraints =
+						getConstraintsForCell(r, c, parent, cols);
+				constraints.setY(y);
+				constraints.setHeight(height);
+			}
+			y = Spring.sum(y, Spring.sum(height, Spring.constant(yPad)));
+		}
+
+		//Set the parent's size.
+		SpringLayout.Constraints pCons = layout.getConstraints(parent);
+		pCons.setConstraint(SpringLayout.SOUTH, y);
+		pCons.setConstraint(SpringLayout.EAST, x);
+	}
+	public static void main(String[] args) {
+		CreateAccountGUI b = new CreateAccountGUI();
+		b.setVisible(true);
+	}
+
 }
