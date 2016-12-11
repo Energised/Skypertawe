7d8d1c6db7d527880f1e15bbd8bc5b0bd7cd2dc2
Ricky3038 <ricky3038@live.co.uk>
2016-12-11 15:11:58 +0000

    Update Vertex.java

--- readwrite/Vertex.java
+++ readwrite/Vertex.java
@@ -1,18 +1,32 @@
 import java.util.ArrayList;
 
+/**
+* The Vertex class will be used in the Graph and Edge classes. It also uses the Edge class.
+*  A vertex has a username to identify it by, and an arraylist of edges that it is involved in
+*
+* @author  830169
+* @version 1.0 
+*/
+
 public class Vertex {
 	//arraylist of edges connected to this vertex
 	private ArrayList<Edge> edges;
 	//username used to identify which account this vertex represents
 	private String username;
 	
+	/**
+	 * constructor for a vertex
+	 * @param username the username that corresponds to the vertex
+	 */
 	public Vertex(String username){
 		this.username = username;
 		this.edges = new ArrayList<Edge>();
 	}
 	
-	
-	//add an edge to arraylist
+	/**
+	 * add an edge to the vertex's arraylist of edges that its involved in
+	 * @param edge the edge to add
+	 */
 	public void addEdge(Edge edge){
 		if (this.edges.contains(edge)){
 			return;
@@ -21,35 +35,52 @@ public class Vertex {
 	}
 	
 	
-	
-	//does this edge exist in the arraylist
+	/**
+	 * check if the edges arraylist contains a certain edge
+	 * @param edge the edge being searched for
+	 * @return returns true if the edge exists
+	 */
 	public boolean containsEdge(Edge edge){
 		return this.edges.contains(edge);		
 	}
 	
-	
-	//returns edge at index
+	/**
+	 * return the edge at specified index
+	 * @param index the index of the arraylist that the edge is located at
+	 * @return returns an edge
+	 */
 	public Edge getEdge(int index){
 		return this.edges.get(index);
 	}
 	
-	//remove edge from the arraylist
+	/**
+	 * remove edge from the arraylist
+	 * @param edge edge to be removed
+	 */
 	public void removeEdge(Edge edge){
 		this.edges.remove(edge);
 	}
 	
-	
-	//get number of edges
+	/**
+	 * get number of edges
+	 * @return the number of edges this vertex is involved in
+	 */
 	public int getNumberEdges(){
 		return this.edges.size();		
 	}
 	
-	//returns the username of vertex
+	/**
+	 * returns the username of vertex
+	 * @return username of the vertex
+	 */
 	public String getUsername(){
 		return this.username;
 	}
 	
-	//return arraylist of edges for this vertex
+	/**
+	 * return arraylist of edges for this vertex
+	 * @return arraylist containing all edges this vertex is involved in
+	 */
 	public ArrayList<Edge> getEdges(){
 		return this.edges;
 	}
